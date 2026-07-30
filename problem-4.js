6e9045f59fdbc98b7c6961aeafcb346c93fc6585
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-07-30 20:12:38 +0600

    added all fils and updates fils

---
+++ problem-4.js
@@ -0,0 +1,30 @@
+function topRatedRestaurant(restaurants) {
+    // Write your code 
+    // checking
+    if(!Array.isArray(restaurants) || restaurants.length === 0){
+        return "Invalid"
+    }
+
+let maxRating = restaurants[0];
+
+for(let i = 1; i < restaurants.length; i++){
+    if(restaurants[i].rating > maxRating.rating) {
+        maxRating = restaurants[i];
+    } 
+  
+        
+
+    return maxRating.name.toUpperCase();
+}
+}
+
+
+
+
+
+
+console.log(topRatedRestaurant([{name:"Chillox",rating:4.5},{name:"Sultan's Dine",rating:4.8}]));
+console.log(topRatedRestaurant([{name:"KFC",rating:4.2},{name:"Pizza Hut",rating:4.6}]));
+console.log(topRatedRestaurant([]));
+console.log(topRatedRestaurant("restaurants"));
+console.log(topRatedRestaurant([{"name":"Burger King","rating":3.9}]));
